49e388bd66936676c747be3ad3cb7472103e166d
darrida <darridapy@gmail.com>
2020-03-10 00:48:28 -0500

    Create config_file_utility.py

---
+++ config_file_utility.py
@@ -0,0 +1,42 @@
+from configparser import ConfigParser
+
+
+def config_update(file, section, option, value):
+    """
+    Creates and/or updates a configuration flat file.
+    
+    Args:
+        file (str): file to change or create
+        section (str): config file section to target or create
+        option (str): config file option (under a section) to target or create
+        value (str): value for the targeted option
+    """
+    config = ConfigParser()
+    config.read(file)
+    cfgfile = open(file, 'w')
+    if config.has_section(section) == True:
+        config.set(section, option, value)
+    else:
+        config.add_section(section)
+        config.set(section, option, value)
+    config.write(cfgfile)
+    cfgfile.close()
+
+
+def config_read(file, section, option):
+    """
+    Reads configuration flat file and returns a value.
+    
+    Args:
+        file (str): config file to read
+        section (str): config file section to target
+        option ([type]): config file option to retrieve value from
+    
+    Returns:
+        str: value stored in configuration file
+    """
+    config = ConfigParser()
+    config.read(file)
+    if config.has_option(section, option) == True:
+        value = config.get(section, option)
+    return value
